cbcfe58e1d2eadd20113da1ce763c33b710f02a8
DongvinPark <dongvin99@naver.com>
2024-10-21 23:11:44 +0900

    iterator added

--- 02-datastructure/doubly_linked_list_with_concept.cpp
+++ 02-datastructure/doubly_linked_list_with_concept.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
 using namespace std;
-
+/*
 template<typename T>
 concept PrinnableAndComparable = requires(T a, T b, std::ostream& os)
 {
   {os << a} -> std::same_as<std::ostream&>;
   {a==b} -> std::same_as<bool>;
 };
-
+*/
 template<typename T>
 class Node {
   private:
@@ -28,16 +28,29 @@ class Node {
   void setPrev(Node* p) {prev = p;}
 };
 
-template<PrinnableAndComparable T>
+template</*PrinnableAndComparable*/typename T>
 class DLL {
   private:
   Node<T>* header;
   Node<T>* trailer;
   size_t sz;
 
+  bool addBetween(T e, Node<T>* p, Node<T>* n) {
+      Node<T>* newNode = new Node<T>(e, p, n);
+      p->setNext(newNode);
+      n->setPrev(newNode);
+      sz++;
+      return true;
+  }
+
   public:
   // Constructor
-  explicit DLL() : header{nullptr}, tailer{nullptr}, sz{0} {}
+  explicit DLL() : header{nullptr}, trailer{nullptr}, sz{0} {
+      header = new Node<T>();
+      trailer = new Node<T>();
+      trailer->setPrev(header);
+      header->setNext(trailer);
+  }
 
   // Destructor
 
@@ -45,7 +58,7 @@ class DLL {
 
   // Copy Assignment operator
 
-  // Move Constructor : 컴파일러의 RVO 동작에 의해서 거의 시행되지 않으므로 생략
+  // Move Constructor : 컴파일러의 RVO 동작에 의해서 거의 실행되지 않으므로 생략
 
   // Move Assignment operator
 
@@ -65,17 +78,17 @@ class DLL {
     return trailer->getPrev()->getElem();
   }
 
-  void addFirst(T e) {}
+  void addFirst(T e) {
+      addBetween(e, header, header->getNext());
+  }
 
-  void addLast(T e) {}
+  void addLast(T e) {
+      addBetween(e, trailer->getPrev(), trailer);
+  }
 
   void removeFirst() {}
 
   void removeLast() {}
-
-  void addFirst() {}
-
-  void addLast() {}
 
   void addElemBefore() {}
 
@@ -83,23 +96,66 @@ class DLL {
     return false;
   }
 
-  bool addBetween(T e) {
-    return true;
-  }
-
   void printList() {
-
+      Node<T>* cur = header;
+      while(true){
+          cur = cur->getNext();
+          if(cur->getNext() != nullptr){
+              cout << cur->getElem() << ", ";
+          } else break;
+      }
+      cout << "\n";
   }
 
   void printListReverse() {
-
+      Node<T>* cur = trailer;
+      while(true){
+          cur = cur->getPrev();
+          if(cur->getPrev() != nullptr){
+              cout << cur->getElem() << ", ";
+          } else break;
+      }
+      cout << "\n";
   }
 
   // Iterator
   class Iterator {
+      private:
+      Node<T>* cur;
+
+      public:
+      Iterator(Node<T>* node) : cur{node} {}
+
+      const T& operator*(){
+          return cur->getElem();
+      }
+
+      Iterator& operator++(){
+          if(cur != nullptr) cur = cur->getNext();
+          return *this;
+      }
+
+      // 반복자를 이용한 역방향 순회도 가능하게 함.
+      Iterator& operator--() {
+          if(cur != nullptr) cur = cur->getPrev();
+          return *this;
+      }
+
+      bool operator!=(const Iterator& other){
+          return cur != other.cur;
+      }
 
   };
-};
+
+  Iterator begin() {return Iterator(header->getNext());}
+  Iterator end() {return Iterator(trailer);}
+
+  // 역방향 순회를 위한 함수 정의
+  Iterator rbegin() {return Iterator(trailer->getPrev());}
+  Iterator rend() {return Iterator(header);}
+
+
+};// end of DLL
 
 class MyData {
   public:
@@ -133,8 +189,27 @@ class InvalidData {
 
 };
 
-int main() {
+int main(){
+    
+    DLL<int> first{};
 
+    first.addLast(1);
+    first.addFirst(2);
+    first.addLast(3);
+    first.printList(); // expect : 2, 1, 3
+    first.printListReverse(); // expect : 3, 1, 2
+    
+    cout << "printList by Iterator!!\n";
+    for(auto& elem : first){
+        cout << elem << ", ";
+    }
+    cout << "\n";
 
-  return 0;
+    cout << "printList in reverse order using Itegator!!\n";
+    for(DLL<int>::Iterator it = first.rbegin(); it != first.rend(); --it){
+        cout << *it << ", ";
+    }
+    cout << "\n";
+
+    return 0;
 }// end of main

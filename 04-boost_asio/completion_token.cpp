5b3e7150551c45924b39211b40c9230d7beb60ff
DongvinPark <dongvin99@naver.com>
2024-11-06 22:41:13 +0900

    updated completion token

--- 04-boost_asio/completion_token.cpp
+++ 04-boost_asio/completion_token.cpp
@@ -24,6 +24,12 @@ completion token 의 역할을 이해하기 위한 목적으로 Chat GPT에게 �
 메인 함수에 진입한 직후로부터 약 1초의 시간이 지나면 "Timer completed successfuly."
 또는 "Timer error: ..."라는 메시지가 나온 후 종료 된다.
 
+이 코드에서 주목할 점은, 실제 돌아가는 스레드는 메인 스레드 1개 뿐인데,
+메인 스레드는 async_wait_some() 함수한테 일을 맡겨놓고는 자기는 blocking 되지 않고
+자기가 해야 하는 다른 할 일을 하다가 async_wait_some() 함수가 할 일을 끝내면 그 결과를
+받아서 일을 처리한다는 점이다. 즉, 멀티 스레드의 도움 없이 async 동작을 하고 있는 것이고,
+이것이 boost.asio가 지향하는 '비동기'이다.
+
 boost.asio에서 completion token의 역할을 정확히 알고 싶다면 아래의 문서를 참고한다.
 이 문서는 boost.asio 를 구현한 개발자가 직접 작성한 문서다.
 https://www.boost.org/doc/libs/1_86_0/doc/html/boost_asio/overview/model/completion_tokens.html
@@ -41,13 +47,30 @@ int main()
     async_wait_some(timer, std::chrono::milliseconds(1'000),
         [](const error_code& ec) {
             if (!ec) {
-                std::cout << "Timer completed successfully.\n";
+                // 이게 java의 System.currentTimemillis()의 C++ 버전이다.
+                long milliseconds_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::system_clock::now().time_since_epoch()
+                ).count();
+
+                std::cout << "\nTimer completed successfully. epoch millisec : " << milliseconds_since_epoch << "\n";
             }
             else {
                 std::cout << "Timer error: " << ec.message() << "\n";
             }
         }
     );
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "do other work in main! : ";
+        
+        long milliseconds_since_epoch = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()
+        ).count();
+        std::cout << "Current time in milliseconds since epoch: " << milliseconds_since_epoch << " ms\n";
+
+        // 이게 java의 Thread.sleep();과 비슷한 기능이다.
+        std::this_thread::sleep_for(std::chrono::milliseconds(90));
+    }
 
     io.run();
     return 0;
